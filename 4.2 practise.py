1f4bbf50c9a117ebe5ef988c25353a8a07127c87
Burcu Karabacak <98615256+burcu-karabacak@users.noreply.github.com>
2022-02-04 21:54:05 +0300

    Add files via upload

---
+++ 4.2 practise.py
@@ -0,0 +1,80 @@
+
+"""
+ogrenciNotlari={"Arzu":90,"Erdal":60,"Sumeyra":70,"Dua":90,"Meryem":100,"Yaren":90}
+top=0
+for i,j in ogrenciNotlari.items():
+print("Öğrenci Adı: {0}\t Notu :{1}".format(i,j))
+top+=jprint("Ortalama :{}".format(top/len(ogrenciNotlari)))
+notlar={"erdal":{"Mat":45,"Eng":78,"Phys":78,"Science":77},"arzu":{"Mat":45,"Eng":78,"Phys":78,"Science":77}}
+notlar["erdal"]["Mat"]
+notlar["arzu"]["Eng"]
+
+
+"""
+"""
+öntel={"Acil Çağrı Merkezi":"112","Polis İmdat":"155","Milli Eğitim Bakanlığı İletişim Merkezi":"444 0 632"}
+print(öntel.values())
+print(dir(list))  
+#öntel.pop("Acil Çağrı Merkezi")
+#print(öntel)
+"""
+"""
+öntel={"Acil Çağrı Merkezi":"112","Polis İmdat":"155","Milli Eğitim Bakanlığı İletişim Merkezi":"444 0 632"}
+if "Polis İmdat" in öntel:
+    print("vardır")
+else:
+    print("Kullanıcı yoktur")
+
+print(len("Polis İmdat "))
+#öntel.clear()
+#print(öntel)
+if "Sağlık Bakanlığı İletişim Merkezi" in dict.keys(öntel): # dict.keys(öntel) sözlukteki anahtar kelime var mı diye sorduk
+    print("var") #### dict(öntel) ledeki gibide olabilir
+else:
+    print("yok")
+"""
+"""
+k={"python",'c',4,"cahit","python"}  # sözlüklerde dictionary index desteklemiyor
+#print(len(k))
+#print(dir(set)) # bunlar kullan. veri setlerini gösterir.
+K=list(k)
+print(K)
+print(K[3])
+print(K[0])
+print(K[1])
+print(K[2])
+
+k=["python",'c',4,"cahit","python"] ##
+print(len(k))
+"""
+"""
+a=int(input("sayı giriniz"))
+k={1,2,3,4,5}
+k.add(a)
+topla=0
+for i in k:
+    topla=topla+i
+print(k)
+print(topla)
+
+"""
+"""
+#update
+k1={1,2,3,4,5,6} # k1'e k2 yi ekledi
+k2={7,8,9,10}
+k1.update(k2)
+print(k1)
+k1.remove(4)
+#k1.remove(11) # ******hata verir
+print(k1) #keyeror[11]****
+k1.discard(11) # discard la hata mesajı vermiyor****
+print(k1)
+"""
+
+
+k1={1,2,3,4,5,6} # k1'e k2 yi ekledi
+k2={7,8,9,10,6,5}
+x={545,45,4554,98,26}
+print(k1.union(k2))  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(k1.union(k2.union(x)))#{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 4554, 26, 545, 98, 45}
+
---
+++ 4.2 week.py
@@ -0,0 +1,132 @@
+#a
+"""
+list=["B","İ","L","İ","Ş","İ","M",]
+list.sort()
+print(list)
+#b
+
+list=["B","İ","L","İ","Ş","İ","M",]
+list.reverse()
+print(list)
+
+#c
+
+list=["B","İ","L","İ","Ş","İ","M",]
+print(list.count("İ"))
+
+#ç
+
+list=["B","İ","L","İ","Ş","İ","M",]
+del list[4:6]
+print(list)
+#
+ders=["b","i","l","i","ş","i","m"]
+ders.remove("ş")
+ders.pop(4)
+print(ders)
+#d
+
+list=["B","İ","L","İ","Ş","İ","M",]
+a=list.copy()
+print(a)
+
+#e silme clear
+
+#f
+list=["B","İ","L","İ","Ş","İ","M",]
+print(list.index("L"))
+
+"""
+#1
+"""
+a=[35,26,81,64]
+a.sort()
+print(a)
+ 
+#2
+a=[35,26,81,64]
+a.reverse()
+print(a)
+
+#3
+a=[35,26,81,64]
+print(a.count(26))
+
+#4
+a=[35,26,81,64]
+a.remove(81)
+print(a)
+
+#5 tüm elemanları sil
+#numbers=[35,26,84,64]liste1=[1.4,6.8]liste1 =numbers.extend(liste1)print(numbers)
+
+meyve=["elma","armut"]
+aliste=["peynir","süt",meyve]
+print(len(aliste))  
+print(aliste)      #['peynir', 'süt', ['elma', 'armut']]
+
+for i in aliste:
+    print(i)
+"""
+"""
+bellekler=["RAM", "ROM"]
+ekran_kartlari=["Paylaşımlı", "Paylaşımsız"]
+sabit_diskler=["SSD"]
+birimler=bellekler, ekran_kartlari, sabit_diskler   #(['RAM', 'ROM'], ['Paylaşımlı', 'Paylaşımsız'], ['SSD'])
+print(birimler)
+
+bellekler=["RAM", "ROM"]
+ekran_kartlari=["Paylaşımlı", "Paylaşımsız"] # ['RAM', 'ROM', 'Paylaşımlı', 'Paylaşımsız', 'SSD']
+sabit_diskler=["SSD"]
+birimler=bellekler+ ekran_kartlari+ sabit_diskler
+print(birimler)
+
+
+sozluk = {"Bilim insanı":"Aziz Sancar", "Şair":"Mehmet Akif Ersoy", "Astronom":"Ali Kuşçu" }
+meslek=sozluk.copy()
+print(meslek)
+
+
+sozluk={"Bilim İnsanı":"Aziz SANCAR","Şair":"Mehmet Akif ERSOY","Astronom":"Ali Kuşçu"}
+print(sozluk.values())
+print(sozluk.keys())
+
+sozluk={"Bilim İnsanı":"Aziz SANCAR","Şair":"Mehmet Akif ERSOY","Astronom":"Ali Kuşçu"}
+del sozluk
+print(sozluk)  #not defined uyarısı verir.
+
+
+
+sozluk={"Bilim İnsanı":"Aziz SANCAR","Şair":"Mehmet Akif ERSOY","Astronom":"Ali Kuşçu"}
+sozluk.clear()
+print(sozluk)  #not defined uyarısı verir.
+
+sozluk={"Bilim İnsanı":"Aziz SANCAR","Şair":"Mehmet Akif ERSOY","Astronom":"Ali Kuşçu"}
+sozluk["Matematikçi"]="Cahit ARİF"
+print(sozluk)
+
+
+sozluk={"Bilim İnsanı":"Aziz SANCAR","Şair":"Mehmet Akif ERSOY","Astronom":"Ali Kuşçu"}
+print("sanatçı" in sozluk)
+print(sozluk)
+sozluk["Bilim İnsanı"]="Canan Dağdeviren" # anahtar kelime yoksa yeni ekliyor varsa güncelliyor
+print(sozluk)
+print(sozluk["Şair"])
+
+"""
+notlar={"Erdal":"83","Arzu":"88","Yaren":"48"}
+a=(input("isminizi girin"))
+b=int(input(" notuzu girin"))
+print(notlar["Erdal"])
+notlar[a]=b
+print(notlar)
+#####
+ogrenciNotlari={"Arzu":90,"Erdal":60,"Sumeyra":70,"Dua":90,"Meryem":100,"Yaren":90}
+top=0
+for i,j in ogrenciNotlari.items():
+print("Öğrenci Adı: {0}\t Notu :{1}".format(i,j))
+top+=j
+print("Ortalama :{}".format(top/len(ogrenciNotlari)))
+
+notlar={"erdal":{"Mat":45,"Eng":78,"Phys":78,"Science":77},"arzu":{"Mat":45,"Eng":78,"Phys":78,"Science":77}}
+
